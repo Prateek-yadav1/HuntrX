28e1c6e463ae7a39d5f4442edd5cd4d53aa0bdf7
Sahil <sahil.kumar.23cse@bmu.edu.in>
2025-09-19 15:25:05 +0530

    grid 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import TopBar from "./components/TopBar";
 import Sidebar from "./components/Sidebar";
+import Home_Layout from "./components/home_Layout";
 
 const App = () => {
   const [isOpen, setIsOpen] = useState(true);
@@ -11,6 +12,7 @@ const App = () => {
       <TopBar />
       <div className="flex flex-1 overflow-hidden">
         <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
+        <div className="flex justify-center w-full"><Home_Layout /></div>
       </div>
     </div>
   );
---
+++ src/components/home_Layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home_Layout = () => {
+  return (
+    <div className='container grid gap-5 grid-cols-[240px_1fr_240px] bg-amber-900 p-8 w-full h-screen'>
+    <div className="bg-amber-200">Hi</div>
+     <div className="bg-white">Hi</div>
+     <div className="bg-white">Hi</div>
+    </div>
+  )
+}
+
+export default Home_Layout
