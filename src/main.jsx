b17da0c8272f372961b8e137a89d3ab9e4a0bd2d
Sahil <sahil.kumar.23cse@bmu.edu.in>
2025-09-19 14:28:45 +0530

    _

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Sidebar from "./App";
+import App from "./App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
