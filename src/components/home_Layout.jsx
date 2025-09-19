e74fd9a0b45fba7bb0bfb0dfe5242dc785454a14
Sahil <sahil.kumar.23cse@bmu.edu.in>
2025-09-19 15:49:34 +0530

    intiated profilecard

--- src/components/home_Layout.jsx
+++ src/components/home_Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home_Layout = () => {
   return (
-    <div className='container grid gap-5 grid-cols-[240px_1fr_240px] bg-amber-900 p-8 w-full h-screen'>
+    <div className='container grid gap-5 grid-cols-[360px_1fr_360px] bg-amber-900 p-8 w-full h-screen'>
     <div className="bg-amber-200">Hi</div>
      <div className="bg-white">Hi</div>
      <div className="bg-white">Hi</div>
